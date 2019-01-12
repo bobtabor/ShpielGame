74b6418d85473a598b77cb1399279e7fb4c5f958
Bob Tabor <bobtabor@outlook.com>
2019-01-12 08:47:28 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ShpielGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's Make a Shpiel!");
+            Console.Write("Do you want the prize behind Door 1, 2 or 3? ");
+            string response = Console.ReadLine();
+
+            if (response == "1") {
+                Console.WriteLine("You won a car!");
+            } else if (response == "2") {
+                Console.WriteLine("You won a boat");
+            } else if (response == "3") {
+                Console.WriteLine("You won a cat!");
+            } else {
+                Console.WriteLine("You had one job: 1, 2 or 3 ... you LOSE!");
+                Console.Beep();
+            }
+        }
+    }
+}
